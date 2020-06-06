e4e4c955617de958e3f6d7267b354b65b0acf952
Dylan Dewey <dylandewey41@gmail.com>
2020-06-06 11:03:18 -0600

    create vars for random

--- 02-Homework/Develop/Assets/script.js
+++ 02-Homework/Develop/Assets/script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+/* var passwordLength = prompt ("How many characters are desired? (8-128)");
+  if ((passwordLength > 7) && (passwordLength < 129)) {
+    console.log(passwordLength);
+  }
+    else {alert ("Password must be between 8 - 128 characters")
+      console.log(passwordLength);
+  }*/
+
 var randomfunct = {
   lower: getRandomLower,
   upper: getRandomUpper,
@@ -15,19 +23,22 @@ function passwordLength() {
 }
 
 function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  var lower = "abcdefghijklmnopqrstuvwxyz"
+  return lower [Math.floor(Math.random() * lower.length)];
 }console.log(getRandomLower());
 
 function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  return upper [Math.floor(Math.random() * upper.length)];
 }console.log(getRandomUpper());
 
 function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  var numbers = "0123456789"
+  return numbers [Math.floor(Math.random() * numbers.length)];
 }console.log(getRandomNumber());
 
 function getRandomSymbol() {
-  var symbols = '!@#$%^&*(){}[]=<>/,.'
+  var symbols = "!@#$%^&*(){}[]=<>/,."
   return symbols [Math.floor(Math.random() * symbols.length)];
 }console.log(getRandomSymbol());
 
@@ -36,21 +47,15 @@ function writePassword(lower, upper, number, symbol) {
   var password = generatePassword();
 }
 
-var passwordLength = prompt ("How many characters are desired? (8-128)");
-  if ((passwordLength > 7) && (passwordLength < 129)) {
-    console.log(passwordLength);
-  }
-    else {alert ("Password must be between 8 - 128 characters")
-      console.log(passwordLength);
-  }
+
   
-// var passwordLower = prompt ("Do you want to include lowercase letters?");
+// var passwordLower = confirm ("Do you want to include lowercase letters?");
 
-// var passwordUpper = prompt ("Do you want to include uppercase letters?");
+// var passwordUpper = confirm ("Do you want to include uppercase letters?");
 
-// var passwordNumber = prompt ("Do you want to include numbers?");
+// var passwordNumber = confirm ("Do you want to include numbers?");
 
-// var passwordSymbol = prompt ("Do you want to include symbols?");
+// var passwordSymbol = confirm ("Do you want to include symbols?");
 
   var passwordText = document.querySelector("#password");
 
