3a8a546207c87bba525e589b5eb10b730608a184
Dylan Dewey <dylandewey41@gmail.com>
2020-06-09 12:34:37 -0600

    more cleanup

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,5 +1,4 @@
 // Assignment Code
-
 var lower = "abcdefghijklmnopqrstuvwxyz"
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var number = "0123456789"
@@ -12,9 +11,9 @@ function password(pwdl, characters) {
   var pwd = "";
   for (var i = 0; i < pwdl; i++) {
     pwd += characters.charAt(Math.floor(Math.random() * characters.length));
-  }console.log(pwd)
+  } console.log(pwd)
 
-// For loop to make sure the variables selected by user are included in the password
+  // For loop to make sure the variables selected by user are included in the password
   for (var g = 0; g < forSure.length; g++) {
     // pwd.charAt(g) = forSure.charAt(g)
     // pwd = pwd.replace()
@@ -30,15 +29,15 @@ function randomPassword() {
   var passwordLength = prompt("How many characters are desired? (8-128)");
   console.log(passwordLength);
 
-// While loop if character number is less than 8  or more than 128 
+  // While loop if character number is less than 8  or more than 128 
   while (passwordLength <= 7 || passwordLength >= 129) {
     alert("Password must be between 8 - 128 characters");
     // randomPassword();
     var passwordLength = (prompt("How many characters are desired? (8-128)"));
-   console.log(passwordLength);
+    console.log(passwordLength);
   }
 
-// User selects which type of characters to include in the password 
+  // User selects which type of characters to include in the password 
   var passwordLower = confirm("Do you want to include lowercase letters?");
   if (passwordLower) {
     generate += lower
@@ -68,35 +67,33 @@ function randomPassword() {
   while (passwordLower === false && passwordUpper === false && passwordNumber === false && passwordSymbol === false) {
     alert("You must choose at least one set of characters");
 
-
-    var passwordLower = confirm("Do you want to include lowercase letters?");
-  if (passwordLower) {
-    generate += lower
-    forSure += lower.charAt(Math.floor(Math.random() * lower.length))
-  } console.log(passwordLower);
-
-  //change string to array and use this
+    //change string to array and use this
     //generate = generate.concat(lower);
+    var passwordLower = confirm("Do you want to include lowercase letters?");
+    if (passwordLower) {
+      generate += lower
+      forSure += lower.charAt(Math.floor(Math.random() * lower.length))
+    } console.log(passwordLower);
 
-  var passwordUpper = confirm("Do you want to include uppercase letters?");
-  if (passwordUpper) {
-    generate += upper
-    forSure += upper.charAt(Math.floor(Math.random() * upper.length))
-  } console.log(passwordUpper);
+    var passwordUpper = confirm("Do you want to include uppercase letters?");
+    if (passwordUpper) {
+      generate += upper
+      forSure += upper.charAt(Math.floor(Math.random() * upper.length))
+    } console.log(passwordUpper);
 
-  var passwordNumber = confirm("Do you want to include numbers?");
-  if (passwordNumber) {
-    generate += number
-    forSure += number.charAt(Math.floor(Math.random() * number.length))
-  } console.log(passwordNumber);
+    var passwordNumber = confirm("Do you want to include numbers?");
+    if (passwordNumber) {
+      generate += number
+      forSure += number.charAt(Math.floor(Math.random() * number.length))
+    } console.log(passwordNumber);
 
-  var passwordSymbol = confirm("Do you want to include symbols?");
-  if (passwordSymbol) {
-    generate += symbol
-    forSure += symbol.charAt(Math.floor(Math.random() * symbol.length))
-    console.log(forSure);
-  } console.log(passwordSymbol);
-  
+    var passwordSymbol = confirm("Do you want to include symbols?");
+    if (passwordSymbol) {
+      generate += symbol
+      forSure += symbol.charAt(Math.floor(Math.random() * symbol.length))
+      console.log(forSure);
+    } console.log(passwordSymbol);
+
   }
   document.getElementById("password").value = password(passwordLength, generate);
 }
